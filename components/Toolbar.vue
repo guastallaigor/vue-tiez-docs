6c025c653ff8b006a33c719e76ab52e9754b0cc8
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2019-06-03 22:52:31 -0300

    first draft of the toolbar

---
+++ components/Toolbar.vue
@@ -0,0 +1,120 @@
+<template lang="pug">
+  .header
+    div
+      header
+        aside.nav-upper
+          a.logo
+            <svg width="43" height="38" viewBox="0 0 43 38" aria-label="geist"><g stroke="#000" fill="none" fill-rule="evenodd"><path d="M21 4l17 30H4L21 4z" stroke-dasharray="2,3"></path><rect fill="#FFF" x="17.5" y=".5" width="7" height="7" rx="3.5"></rect><rect fill="#FFF" x="35.5" y="30.5" width="7" height="7" rx="3.5"></rect><rect fill="#FFF" x=".5" y="30.5" width="7" height="7" rx="3.5"></rect></g></svg>
+          .menu-arrow
+        aside.nav-lower
+          .desktop-only
+            .nav.right
+              div
+                span.links
+                  a Blog
+                  a Support
+                  a Docs
+</template>
+
+<style lang="stylus">
+.header
+  position fixed
+  top 0px
+  left 0px
+  right 0px
+  z-index 1
+
+  header
+    justify-content space-between
+    max-width 100%
+    margin 2px 10px
+    width auto
+    position relative
+    transition height 0.3s ease
+    @media (min-width: 951px)
+      padding 10px 20px
+      display flex
+      flex-direction row
+      align-items center
+      height 80px
+
+    .nav-upper
+      display flex
+      justify-content space-between
+      align-items center
+      transition padding 0.3s ease
+      padding 20px 0
+
+      a.logo
+        display block
+        width 36px
+        height 31px
+        position relative
+        padding 3px 4px
+        margin -3px -4px
+        transition height 0.3s ease
+
+      .menu-arrow
+        display flex
+        width 40px
+        height 40px
+        margin -9px
+        align-items center
+        justify-content center
+        transition transform 0.2s ease
+        fill #000
+        @media (min-width: 951px)
+          display none
+
+    .nav-lower
+      display none
+      margin-bottom 30px
+      width 100%
+      @media (min-width: 951px)
+        width auto
+        display flex
+        justify-content space-between
+        align-items center
+        margin -2px 0 0 10px
+        background-color transparent
+
+      .desktop-only
+        display flex
+        justify-content space-between
+        align-items center
+        width 100%
+        visibility visible
+
+        .nav
+          display flex
+          flex-direction column
+          @media (min-width: 951px)
+            display flex
+            flex-direction row
+            animation fade-in 0.2s ease
+            align-items center
+          &.right > div
+            display flex
+            align-items center
+
+            .links
+              display flex
+              align-items center
+              margin 0 9px
+
+              a
+                color #999
+                font-size 14px
+                text-decoration none
+                padding 0 9px
+                transition color 0.2s ease
+
+        .right
+          right 0
+
+@keyframes fade-in
+  0%
+    opacity 0
+  100%
+      opacity 1
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,56 @@
-import pkg from './package'
+const isProd = process.env.NODE_ENV === 'production'
 
-export default {
+module.exports = {
   mode: 'spa',
+  cache: true,
 
   /*
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    htmlAttrs: {
+      lang: 'pt-BR'
+    },
+    noscript: [{ innerHTML: 'This website needs JavaScript' }],
+    title: 'Vue Tiez',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Vue Tiez' },
+      { name: 'title', content: 'Vue Tiez' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'robots', content: 'index,follow' },
+      { name: 'keywords', content: 'vue,tiez,design,system,component,framework,vuejs' },
+      // Open Graph
+      { name: 'og:title', content: 'Vue Tiez' },
+      { name: 'og:description', content: 'Vue Tiez' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:url', content: 'https://guastallaigor.now.sh' },
+      { name: 'og:image', content: 'https://guastallaigor.now.sh/favicon.png' },
+      { name: 'og:locale', content: 'pt_BR' },
+      // Twitter Card
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@guastallaigor' },
+      { name: 'twitter:title', content: 'Vue Tiez' },
+      { name: 'twitter:description', content: 'Vue Tiez' },
+      { name: 'twitter:image', content: 'https://guastallaigor.now.sh/favicon.png' },
+      { name: 'twitter:image:alt', content: 'Logo Igor Guastalla' },
+      { name: 'theme-color', content: '#ffffff' },
+      { name: 'google-site-verification', content: 'YrK6btveUmKHAC-LVZstuNXNO08QblqJvyAZ0fGEU-o' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' }
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#895DE9', height: '10px' },
 
   /*
   ** Global CSS
@@ -39,7 +68,20 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    'nuxt-purgecss',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-133326737-2',
+      disabled: () => document.cookie.indexOf('ga_optout=true') !== -1,
+      debug: {
+        sendHitTask: isProd
+      },
+      set: [
+        { field: 'anonymizeIp', value: true }
+      ]
+    }]
+  ],
+  vendor: [
   ],
 
   /*
@@ -50,6 +92,14 @@ export default {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
+      [].concat(...config.module.rules.find(e => e.test.toString().match(/\.styl/)).oneOf.map(e => e.use.filter(e => e.loader == 'stylus-loader'))).forEach(stylus => {
+        Object.assign(stylus.options, {
+          import: [
+            '~rupture/rupture/index.styl'
+          ]
+        })
+      })
+
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
