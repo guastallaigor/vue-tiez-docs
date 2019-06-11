495ffd53515ced9239362b7ac1e789723a03ad83
Igor Guastalla de Lima <igor.lima@sabium.com.br>
2019-06-11 12:02:16 -0300

    add show more doc, found more bugs

---
+++ code/show-more.js
@@ -0,0 +1,16 @@
+export default {
+  showMore: `\n
+<div style="padding: 20px 20px 10px 0px;">
+  <z-show-more-toggle more/>
+</div>
+<div style="padding: 20px 20px 40px 0px;">
+  <z-show-more-toggle />
+</div>`,
+  showMoreDark: `
+<div style="padding: 20px 20px 10px 0px;">
+  <z-show-more-toggle more dark/>
+</div>
+<div style="padding: 20px 20px 40px 0px;">
+  <z-show-more-toggle dark/>
+</div>`
+}
--- pages/show-more.vue
+++ pages/show-more.vue
@@ -1,9 +1,60 @@
 <template lang="pug">
-  div
-    h2 Show more
+  div(v-if="code")
+    h2 Show More
+    p Show More Toggle
+    .module
+      render-string(:string="code.showMore")
+      p.show-hide-code(@click="hiddenShowMore = !hiddenShowMore") </>
+      collapse-transition.width-transition
+        .overflow(v-show="!hiddenShowMore")
+          .relative
+            p.copy(@click="copy(code.showMore, $el)") copy
+            pre
+              code.language-javascript {{ code.showMore }}
+    p Show More Toggle - Dark
+    .module.inverted
+      render-string(:string="code.showMoreDark")
+      p.show-hide-code(@click="hiddenShowMoreDark = !hiddenShowMoreDark") </>
+      collapse-transition.width-transition
+        .overflow(v-show="!hiddenShowMoreDark")
+          .relative
+            p.copy(@click="copy(code.showMoreDark, $el)") copy
+            pre
+              code.language-javascript {{ code.showMoreDark }}
+    p Props
+    .module
+      z-table(:headers="headers" :items="items")
 </template>
-
 <script>
+import code from '~/code/show-more'
+import { CollapseTransition } from 'vue2-transitions'
+import copy from '~/util/copy'
+import headers from '~/util/headers'
+
 export default {
+  components: { CollapseTransition },
+  mixins: [copy],
+  data: () => ({
+    code,
+    hiddenShowMore: true,
+    hiddenShowMoreDark: true,
+    headers,
+    items: [
+      {
+        name: 'more',
+        type: 'Boolean',
+        required: 'false',
+        default: 'false',
+        description: 'If true then change the text to \'Show more\', otherwise it will be \'Show less\''
+      },
+      {
+        name: 'dark',
+        type: 'Boolean',
+        required: 'false',
+        default: 'false',
+        description: 'Set show more toggle to dark mode'
+      }
+    ]
+  })
 }
 </script>
